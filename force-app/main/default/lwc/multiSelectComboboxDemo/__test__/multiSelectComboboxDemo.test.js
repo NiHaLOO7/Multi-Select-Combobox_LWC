d05c731aefc2e0827841d41c82f699a57448b963
Nihal Jha <nihal.jha.5797@gmail.com>
2022-04-11 11:29:04 +0530

    multiselect combobox demo test changes

---
+++ force-app/main/default/lwc/multiSelectComboboxDemo/__test__/multiSelectComboboxDemo.test.js
@@ -0,0 +1,30 @@
+import { createElement } from "lwc";
+import MultiSelectComboboxDemo from "c/multiSelectComboboxDemo";
+
+describe("c=multiselectCombobox test suite", () => {
+  function flushPromises() {
+    return new Promise((resolve) => setImmediate(resolve));
+  }
+
+  it("test initial value of pills, disable, zeroSelectionFlag", () => {
+    const element = createElement("c-multi-select-combobox-demo", {
+      is: MultiSelectComboboxDemo
+    });
+    document.body.appendChild(element);
+    const options = [
+      { label: "one", value: "1" },
+      { label: "two", value: "2" },
+      { label: "three", value: "3" },
+      { label: "four", value: "4" }
+    ];
+    return flushPromises().then(() => {
+      const cmboBox = element.shadowRoot.querySelector(
+        "c-multiselect-combobox"
+      );
+      expect(cmboBox.disabled).toBe(false);
+      expect(cmboBox.pills).toBe(true);
+      expect(cmboBox.zeroSelectionAllowed).toBe(false);
+      expect(cmboBox.options).toEqual(options);
+    });
+  });
+});
