cf1bcefcce4fe1b74e38c7d51a69b2a113ef7fd3
Nihal Jha <nihal.jha.5797@gmail.com>
2022-04-27 14:10:35 +0530

    config file changes for mutation

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
-const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
+const { jestConfig } = require("@salesforce/sfdx-lwc-jest/config");
 
 module.exports = {
-    ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+  ...jestConfig,
+  collectCoverageFrom: [
+    "force-app/main/default/lwc/**/*.js",
+    "!**/node_modules/**"
+  ],
+  moduleNameMapper: {
+    "^c/(.*)": "<rootDir>/force-app/main/default/lwc/$1/$1.js"
+  },
+  modulePathIgnorePatterns: ["<rootDir>/.localdevserver"]
 };
---
+++ stryker.conf.js
@@ -0,0 +1,30 @@
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+const { jestConfig } = require("./jest.config.js");
+module.exports = {
+  _comment:
+    "This config was generated using 'stryker init'. Please take a look at: https://stryker-mutator.io/docs/stryker-js/configuration/ for more information",
+  packageManager: "npm",
+  testRunner: "jest",
+  mutator: "javascript",
+  coverageAnalysis: "off",
+  timeoutMS: 60000,
+  tempDirName: "stryker-tmp",
+  files: [
+    "*",
+    "force-app/main/default/lwc/*",
+    "force-app/main/default/lwc/**/*"
+  ],
+  mutate: [
+    "force-app/main/default/lwc/**/*.js",
+    "!force-app/main/default/lwc/**/__tests__/*.*"
+  ],
+  reporters: ["html", "clear-text", "dots", "progress"],
+  thresholds: { high: 90, low: 85, break: 90 },
+  jest: {
+    projectType: "custom",
+    config: jestConfig,
+    enableFindRelatedTests: true
+  }
+};
